c53b4e79856c80eaebe638484cb7c919e7a4bce7
Kaustav Chakraborty <kaustav.ron@gmail.com>
2017-02-13 12:36:36 +0530

    Kaustav - #13 - Changed travis ci ruby version to 2.2.2

--- scripts/initialize-database.sh
+++ scripts/initialize-database.sh
@@ -4,6 +4,6 @@ CORE_PATH=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")scripts
 DATABASE_PATH=$CORE_PATH/initialize-database.sql
 
 echo "******************* Setting up initial database *******************"
-mysql -uroot -p < ./scripts/initialize-database.sql
+mysql -uroot < ./scripts/initialize-database.sql
 
 echo "******************* Database setup succeeded *******************"
